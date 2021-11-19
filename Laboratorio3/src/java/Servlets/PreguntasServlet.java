9f0724edf8e0a5cb01e19c6c3a97879796ac0606
sebastianmga <61295332+sebastianmga@users.noreply.github.com>
2021-11-19 14:01:59 -0600

    RespuestaServelet

--- Laboratorio3/src/java/Servlets/PreguntasServlet.java
+++ Laboratorio3/src/java/Servlets/PreguntasServlet.java
@@ -7,10 +7,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author bryan
- */
 @WebServlet(name = "PreguntasServlet", urlPatterns = {"/PreguntasServlet"})
 public class PreguntasServlet extends HttpServlet {
 
--- Laboratorio3/src/java/Servlets/RespuestaServlet.java
+++ Laboratorio3/src/java/Servlets/RespuestaServlet.java
@@ -1,16 +1,16 @@
 package Servlets;
 
+import Model.Respuesta;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author Sebastian
- */
 @WebServlet(name = "RespuestaServlet", urlPatterns = {"/RespuestaServlet"})
 public class RespuestaServlet extends HttpServlet {
 
@@ -22,12 +22,40 @@ public class RespuestaServlet extends HttpServlet {
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
+     * @throws java.lang.ClassNotFoundException
+     * @throws java.lang.InstantiationException
+     * @throws java.lang.IllegalAccessException
+     * @throws java.sql.SQLException
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException, ClassNotFoundException, InstantiationException, 
+            IllegalAccessException, SQLException {
         response.setContentType("text/html;charset=UTF-8");
         
-        // Próximamente c:
+        String[] values = request.getParameter("btnActionRespuesta").split("-");
+        int action = Integer.parseInt(values[0]);
+        int id = Integer.parseInt(values[1]);
+        
+        switch (action) {
+            
+            // Insert
+            case 1:
+                Respuesta respuesta = new Respuesta();
+                respuesta.preguntaId = Integer.parseInt(request.getParameter("txtPreguntaId"));
+                respuesta.respuesta = request.getParameter("txtRespuesta");
+                Respuesta.Insert(respuesta);
+                
+                response.sendRedirect("/Laboratorio3/...");
+                break;
+            
+            // Delete
+            case 2:
+                Respuesta.Remove(id);
+                response.sendRedirect("/Laboratorio3/...");
+                break;
+            default:
+                throw new AssertionError();
+        }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -42,7 +70,11 @@ public class RespuestaServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException ex) {
+            Logger.getLogger(RespuestaServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -56,7 +88,11 @@ public class RespuestaServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException ex) {
+            Logger.getLogger(RespuestaServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
