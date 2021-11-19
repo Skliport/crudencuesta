18a9e9ce3edfb33f25a303279b475d7e0310d059
sebastianmga <61295332+sebastianmga@users.noreply.github.com>
2021-11-19 15:23:10 -0600

    RespuestaUsuario

---
+++ Laboratorio3/src/java/Model/RespuestaUsuario.java
@@ -0,0 +1,63 @@
+package Model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class RespuestaUsuario {
+    
+    public int usuarioRespuestaId, respuestaId, usuarioId;
+    public String respuesta;
+    static Conexion oCon;
+    
+    public static ArrayList<RespuestaUsuario> GetAll() throws ClassNotFoundException, 
+        InstantiationException, IllegalAccessException, SQLException{
+        
+        oCon = new Conexion();    
+        ArrayList<RespuestaUsuario> lRespuestaUsuario = new ArrayList<>();
+        ResultSet rs = oCon.consultar("*", "tblRespuestaUsuario");
+        
+        while(rs.next()){
+            RespuestaUsuario respuestaUsuario = new RespuestaUsuario();
+            respuestaUsuario.usuarioRespuestaId = rs.getInt(1);
+            respuestaUsuario.respuestaId = rs.getInt(2);
+            respuestaUsuario.usuarioId = rs.getInt(3);
+            respuestaUsuario.respuesta = rs.getString(4);
+            lRespuestaUsuario.add(respuestaUsuario);
+        }
+        return lRespuestaUsuario;
+    }
+    
+    public static RespuestaUsuario GetById(int usuarioRespuestaId) throws ClassNotFoundException, 
+        InstantiationException, IllegalAccessException, SQLException{
+        
+        oCon = new Conexion();
+        ResultSet rs = oCon.consultar("*", "tblRespuestaUsuario", "usuarioRespuestaId="+usuarioRespuestaId);
+        RespuestaUsuario respuestaUsuario = new RespuestaUsuario();
+        
+        while(rs.next()){
+            respuestaUsuario.usuarioRespuestaId = rs.getInt(1);
+            respuestaUsuario.respuestaId = rs.getInt(2);
+            respuestaUsuario.usuarioId = rs.getInt(3);
+            respuestaUsuario.respuesta = rs.getString(4);
+        }
+        return respuestaUsuario;
+    }
+    
+    public static int Insert(RespuestaUsuario respuestaUsuario) throws ClassNotFoundException, 
+        InstantiationException, IllegalAccessException, SQLException{
+        
+        oCon = new Conexion();
+        int resp = oCon.agregar("Insert into tblRespuestaUsuario(usuarioRespuestaId, respuestaId, usuarioId, respuesta)"
+            + " values('default','"+respuestaUsuario.respuestaId+"','"+respuestaUsuario.usuarioId+"','"
+            +respuestaUsuario.respuesta+"')");
+        return resp;
+    }
+    
+    public static int Remove(int usuarioRespuestaId) throws ClassNotFoundException, 
+        InstantiationException, IllegalAccessException, SQLException{
+        
+        oCon = new Conexion();
+        return oCon.eliminar("tblRespuestaUsuario", "usuarioRespuestaId="+usuarioRespuestaId);
+    }
+}
---
+++ Laboratorio3/src/java/Servlets/RespuestaUsuarioServlet.java
@@ -0,0 +1,90 @@
+package Servlets;
+
+import Model.RespuestaUsuario;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "RespuestaUsuarioServlet", urlPatterns = {"/RespuestaUsuarioServlet"})
+public class RespuestaUsuarioServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> 
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     * @throws java.lang.ClassNotFoundException
+     * @throws java.lang.InstantiationException
+     * @throws java.lang.IllegalAccessException
+     * @throws java.sql.SQLException
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        RespuestaUsuario respuestaUsuario = new RespuestaUsuario();
+        respuestaUsuario.respuestaId = Integer.parseInt(request.getParameter("txtRespuestaId"));
+        respuestaUsuario.usuarioId = Integer.parseInt(request.getParameter("txtUsuarioId"));
+        respuestaUsuario.respuesta = request.getParameter("txtRespuesta");
+
+        RespuestaUsuario.Insert(respuestaUsuario);
+        response.sendRedirect("/Laboratorio3/...");      
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException ex) {
+            Logger.getLogger(RespuestaUsuarioServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException ex) {
+            Logger.getLogger(RespuestaUsuarioServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
